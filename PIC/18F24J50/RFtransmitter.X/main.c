fdfb44db524dfbf0e56376f0af31093033d40dc5
Freddy Lee <fredlee590@gmail.com>
2015-11-04 21:17:32 -0800

    Basic SPI scaffolding.

--- PIC/18F24J50/RFtransmitter.X/main.c
+++ PIC/18F24J50/RFtransmitter.X/main.c
@@ -10,7 +10,6 @@
  */
 
 #include <p18f24j50.h>
-#include <usart.h>
 #include <spi.h>
 
 #pragma config WDTEN = OFF
@@ -21,42 +20,15 @@
 #define LENGTH      3
 #define READ_CMD    0b00000000
 #define WRITE_CMD   0b00100000
+#define NOP_CMD     0b11111111
 
-
-
-// send a single character
-void sendCh(char char_to_send)
+unsigned char readStatus()
 {
-    putc1USART(char_to_send);
-    while(Busy1USART());
-}
+    unsigned char result;
 
-// send a whole string
-void sendStr(rom char* str_to_send)
-{
-    while(*str_to_send)
-    {
-        sendCh(*str_to_send);
-        str_to_send++;
-    }
-    
-    sendCh('\n');
-    sendCh('\r');
-}
-
-unsigned char readStatusFromReg(unsigned char addr)
-{
-    unsigned char cmd, result;
-    // Make sure address only takes least significant 5 bits
-    if(addr & 11100000)
-        return 0;
-    
-    // prepare to send
-    cmd = addr | READ_CMD;
     LATAbits.LATA2 = 0;
     
-    // address goes first
-    WriteSPI1(addr);
+    WriteSPI1(NOP_CMD);
     while(!DataRdySPI1());
     result = ReadSPI1();
 
@@ -65,59 +37,74 @@ unsigned char readStatusFromReg(unsigned char addr)
     return result;
 }
 
-unsigned char* talkToTransceiver(unsigned char cmdType, unsigned char addr,
-        unsigned char* data, unsigned char len)
+unsigned char writeReg(unsigned char addr, unsigned char* data,
+        unsigned char len)
 {
-    unsigned char i, cmd;
-    unsigned char* result;
-
+    unsigned char cmd, i, result;
     // Make sure address only takes least significant 5 bits
     if(addr & 11100000)
         return 0;
     
+    if(len < 1)
+        return 0;
+    
     // prepare to send
-    cmd = addr | cmdType;
+    cmd = addr | WRITE_CMD;
     LATAbits.LATA2 = 0;
     
     // address goes first
-    WriteSPI1(addr);
+    WriteSPI1(cmd);
     while(!DataRdySPI1());
-    ReadSPI1();
+    result = ReadSPI1();
 
     for(i = 0; i < len; i++)
     {
-        //sendCh(data[i] + '0');
-        WriteSPI1(data[i]);
+        WriteSPI(data[i]);
         while(!DataRdySPI1());
-        result[i] = ReadSPI1();
+        ReadSPI1();
     }
+    
     LATAbits.LATA2 = 1;
     
     return result;
 }
 
-void powerOnRF24L01()
+unsigned char readReg(unsigned char addr, unsigned char* data,
+        unsigned char len)
 {
-    //unsigned char powerOnValue = 0b00001010;
-    unsigned char val = 0b00001010;
-    unsigned char* res;
+    unsigned char cmd, i, result;
+    // Make sure address only takes least significant 5 bits
+    if(addr & 11100000)
+        return 0;
+    
+    if(len < 1)
+        return 0;
+    
+    // prepare to send
+    cmd = addr | READ_CMD;
+    LATAbits.LATA2 = 0;
+    
+    // address goes first
+    WriteSPI1(cmd);
+    while(!DataRdySPI1());
+    result = ReadSPI1();
 
-    talkToTransceiver(WRITE_CMD, 0x00, &val, 1);
-    res = talkToTransceiver(READ_CMD, 0x00, 0x0, 1);
-    /*
-    if(!res)
+    for(i = 0; i < len; i++)
     {
-        sendStr("NULL res");
-    } else {
-        sendCh(*res);
+        WriteSPI(0);
+        while(!DataRdySPI1());
+        data[i] = ReadSPI1();
     }
-    */
-        
+
+    LATAbits.LATA2 = 1;
+    
+    return result;
 }
 
 // program goes here
 void main(void)
 {
+    unsigned char* data;
     OSCCONbits.IRCF = 0b111; // max clock speed: 8MHz
     
     // define GPIO for IRQ, CSN, CE for RF module
@@ -130,36 +117,19 @@ void main(void)
     TRISBbits.TRISB5 = 1; // input from MISO on RF transceiver
     TRISCbits.TRISC7 = 0; // output from MOSI to RF transceiver
 
-    /*
-    Open1USART(USART_TX_INT_OFF
-            & USART_RX_INT_OFF
-            & USART_ADDEN_OFF
-            & USART_BRGH_LOW
-            & USART_SINGLE_RX
-            & USART_SYNC_SLAVE
-            & USART_EIGHT_BIT
-            & USART_ASYNCH_MODE, 12);
-     */
     OpenSPI1(SPI_FOSC_64, MODE_00, SMPMID);
 
     LATAbits.LATA0 = 0; // LED
-    LATAbits.LATA2 = 0; // CSN
+    LATAbits.LATA2 = 1; // CSN
     LATAbits.LATA3 = 1; // CE
 
-    //sendStr("Program goes here");
-    
-    if(readStatusFromReg(0x00) & 0x80)
+    //if(readStatus())
+    if(readReg(0x00, data, 1) & 0x8)
         LATAbits.LATA0 = 1;
     
-    /*
-    while(1)
-    {
-        //sendStr("Start SPI command");
-        powerOnRF24L01();
-        //sendStr("SPI command end");
-    }
-    */
-    //Close1USART();
+    if(*data & 0x8)
+        LATAbits.LATA0 = 0;
+    
     CloseSPI1();
 
     while(1);
