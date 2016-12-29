103bfa64e9e0e728511794caa1407b67e9fe883c
Freddy Lee <fredlee590@gmail.com>
2016-12-28 23:31:06 -0800

    Streamline copy_eagle.sh to use unified variables.

--- PCB/copy_eagle.sh
+++ PCB/copy_eagle.sh
@@ -10,30 +10,33 @@ fi
 proj=$1
 
 eagledir="/c/Users/Frederick/Documents/eagle"
+projdir="eagle_projects/$proj/eagle_files"
+devdir="$projdir/dev"
+proddir="$projdir/prod"
 
 mkdir -p $proj/eagle_files/dev &> /dev/null
 mkdir -p $proj/eagle_files/prod &> /dev/null
 
-cp $eagledir/$proj/eagle.epf $proj/eagle_files/dev
-cp $eagledir/$proj/$proj.sch $proj/eagle_files/dev
-cp $eagledir/$proj/$proj.brd $proj/eagle_files/dev
+cp $eagledir/$proj/eagle.epf $devdir
+cp $eagledir/$proj/$proj.sch $devdir
+cp $eagledir/$proj/$proj.brd $devdir
 
 # bottom layer, silkscreen, and solder mask
-cp $eagledir/$proj/$proj\.GBL $proj/eagle_files/prod
-cp $eagledir/$proj/$proj\.GBO $proj/eagle_files/prod
-cp $eagledir/$proj/$proj\.GBS $proj/eagle_files/prod
+cp $eagledir/$proj/$proj\.GBL $proddir
+cp $eagledir/$proj/$proj\.GBO $proddir
+cp $eagledir/$proj/$proj\.GBS $proddir
 
 # top layer, silkscreen, and solder mask
-cp $eagledir/$proj/$proj\.GTL $proj/eagle_files/prod
-cp $eagledir/$proj/$proj\.GTO $proj/eagle_files/prod
-cp $eagledir/$proj/$proj\.GTS $proj/eagle_files/prod
+cp $eagledir/$proj/$proj\.GTL $proddir
+cp $eagledir/$proj/$proj\.GTO $proddir
+cp $eagledir/$proj/$proj\.GTS $proddir
 
 # drill information and outline
-cp $eagledir/$proj/$proj\.GML $proj/eagle_files/prod
-cp $eagledir/$proj/$proj\.TXT $proj/eagle_files/prod
-cp $eagledir/$proj/$proj\.dri $proj/eagle_files/prod
-cp $eagledir/$proj/$proj\.dxf $proj/eagle_files/prod
+cp $eagledir/$proj/$proj\.GML $proddir
+cp $eagledir/$proj/$proj\.TXT $proddir
+cp $eagledir/$proj/$proj\.dri $proddir
+cp $eagledir/$proj/$proj\.dxf $proddir
 
 # specific to OSHPark
-cp $eagledir/$proj/$proj\.GKO $proj/eagle_files/prod
-cp $eagledir/$proj/$proj\.XLN $proj/eagle_files/prod
+cp $eagledir/$proj/$proj\.GKO $proddir
+cp $eagledir/$proj/$proj\.XLN $proddir
